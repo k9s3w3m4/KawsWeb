29f5874c38c3718695ff15e3af44e51aee15777c
k9s3w3m4 <xcxvvv@sina.com>
2017-03-14 19:05:32 +0800

    20170314 commit

--- KawsWebEnter/urls.py
+++ KawsWebEnter/urls.py
@@ -2,9 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
      url(r'^$', views.index,name='index'),
-#     url(r'^(?P<id>[0-9]+)/$',views.idshow,name='idshow'),
-#     url(r'^(?P<name>[0-9]+)/nameshow',views.nameshow,name='nameshow'),
-#     url(r'^(?P<id>[0-9]+)/phoneshow',views.phone,name='phone'),
+     # url(r'^images/(?p<path>.*)','Django.views.static.serve',{'document_root':'~/PycharmProjects/KawsWeb/KawsWebEnter/images'}),
      url(r'^index2/$', views.index2,name='index2'),
      url(r'^index3/$', views.index3,name='index3'),
      url(r'^testlinkjump/$', views.testlinkjump,name='testlinkjump'),
